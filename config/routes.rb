c650d5939611ab3d42d8f0888dca7455cd048e2c
nehamaria <[nehamariasanil@gmail.com]>
2021-10-16 19:39:18 +0530

    Added optimized slug generation method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@
 
 Rails.application.routes.draw do
 
-  resources :tasks, only: :index, param: :slug
-  root "home#index"
-  get "*path", to: "home#index", via: :all
+  resources :tasks, only: %i[index create], param: :slug
 end
